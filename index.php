cefbc208b90b0c4a6d0d2073aafd8681956ca049
adrozdek <agata.drozdek@gmail.com>
2016-01-29 12:44:54 +0100

    Dodanie podstawowych plikow z .gitignore

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Admin
+ * Date: 2016-01-29
+ * Time: 12:00
+ */
---
+++ src/config.php
@@ -0,0 +1,10 @@
+<?php
+
+$dbUsername = "root";
+$dbPassword = "coderslab";
+$dbHost = "localhost";
+$dbName = "tweeter";
+
+
+
+?>
